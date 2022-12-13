c729e13ff76143edbd194365933646c017bc4e1f
MaxiBal <maxibal@baltel.com>
2022-12-13 17:15:55 -0500

    initialize firebase and add login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navigation/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'schoolfair-9b858',
+    appId: '1:566329469840:web:1e216cd96166d85ff62115',
+    storageBucket: 'schoolfair-9b858.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyABpcpuZnZsPzdZdda6gVfpFVYIM-nf72o',
+    authDomain: 'schoolfair-9b858.firebaseapp.com',
+    messagingSenderId: '566329469840',
+  },
   production: true
 };
