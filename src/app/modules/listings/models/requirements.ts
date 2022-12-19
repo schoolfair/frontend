34695252cbca2a49879685c95c33fb885c0be76c
MaxiBal <maxibal@baltel.com>
2022-12-19 16:27:56 -0500

    update listing component

---
+++ src/app/modules/listings/models/requirements.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Represents the requirements for applying to a Listing
+ */
+export interface Requirements {
+  resume?: boolean;
+
+  /**
+   * The applicant's statement of interest
+   */
+  interestStatement?: boolean;
+
+  essays?:boolean;
+}
